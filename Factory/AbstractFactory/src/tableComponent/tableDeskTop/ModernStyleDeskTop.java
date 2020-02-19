1769225231b210f4c8afb995f45ea61e78825b25
ctyFL <502651020@qq.com>
2020-02-19 11:09:22 +0800

    抽象工厂

--- Factory/AbstractFactory/src/tableComponent/tableDeskTop/ModernStyleDeskTop.java
+++ Factory/AbstractFactory/src/tableComponent/tableDeskTop/ModernStyleDeskTop.java
@@ -3,7 +3,7 @@ package tableComponent.tableDeskTop;
 public class ModernStyleDeskTop extends DeskTop {
 
 	public ModernStyleDeskTop() {
-		System.out.println("现代风格桌面完成");
+		System.out.println("现代风格桌面完成");；
 	}
 	
 }
